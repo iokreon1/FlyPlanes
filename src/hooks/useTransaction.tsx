fa3a2f40f57f9a82eaf0e0ec73438255032de553
iokreon1 <wahyuhariyanto157@gmail.com>
2025-11-04 13:50:35 +0700

    sek

--- src/hooks/useTransaction.tsx
+++ src/hooks/useTransaction.tsx
@@ -1,87 +1,94 @@
-// import type { User } from "lucia";
-// import useCheckoutData from "./useCheckoutData";
-// import { useMemo, useState } from "react";
-// import { SEAT_VALUES, type SeatValuesType } from "@/lib/utils";
-// import { useMutation } from "@tanstack/react-query";
-// import axios from "axios";
-// import { useRouter } from "next/navigation";
+import type { User } from "lucia";
+import useCheckoutData from "./useCheckoutData";
+import { useMemo, useState } from "react";
+import { SEAT_VALUES, type SeatValuesType } from "@/lib/utils";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
-// type Props = {
-// 	user: User | null;
-// };
+type Props = {
+	user: User | null;
+};
 
-// const useTransaction = ({ user }: Props) => {
-// 	const { data } = useCheckoutData();
+const useTransaction = ({ user }: Props) => {
+	const { data } = useCheckoutData();
 
-// 	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
 
-// 	const router = useRouter();
+	const router = useRouter();
 
-// 	const selectedSeat = useMemo(() => {
-// 		return SEAT_VALUES[(data?.seat as SeatValuesType) ?? "ECONOMY"];
-// 	}, [data?.seat]);
+	const selectedSeat = useMemo(() => {
+		return SEAT_VALUES[(data?.seat as SeatValuesType) ?? "ECONOMY"];
+	}, [data?.seat]);
 
-// 	const transactionMutate = useMutation({
-// 		mutationFn: (data: any) =>
-// 			axios
-// 				.post("/api/transactions/create", data)
-// 				.then((res) => res.data),
-// 	});
+type TransactionResponse = {
+	midtrans: {
+		token: string;
+	};
+};
 
-// 	const payTransaction = async () => {
-// 		if (!data && !user) {
-// 			return null;
-// 		}
+const transactionMutate = useMutation<TransactionResponse, unknown, any>({
+	mutationFn: async (data) => {
+		const res = await axios.post<TransactionResponse>("/api/transactions/create", data);
+		return res.data;
+	},
+});
 
-// 		const totalPrice = Number(
-// 			data?.flightDetail?.price ?? 0 + selectedSeat.additionalPrice
-// 		);
 
-// 		const bodyData = {
-// 			bookingDate: new Date(),
-// 			customerId: user?.id,
-// 			flightId: data?.flightDetail?.id,
-// 			price: totalPrice,
-// 			seatId: data?.seatDetail?.id,
-// 			departureCityCode: data?.flightDetail?.departureCityCode,
-// 			destinationCityCode: data?.flightDetail?.destinationCityCode,
-// 		};
+	const payTransaction = async () => {
+		if (!data && !user) {
+			return null;
+		}
 
-// 		try {
-// 			setIsLoading(true);
-// 			const transaction = await transactionMutate.mutateAsync(bodyData);
+		const totalPrice = Number(
+			data?.flightDetail?.price ?? 0 + selectedSeat.additionalPrice
+		);
 
-// 			// handle midtrans
-// 			window.snap.pay(transaction.midtrans.token, {
-// 				onSuccess: (result: unknown) => {
-// 					console.log(result);
-// 					router.push("/success-checkout");
-// 				},
-// 				onPending: (result: unknown) => {
-// 					console.log(result);
-// 					router.push("/success-checkout");
-// 				},
-// 				onError: (result: unknown) => {
-// 					console.log(result);
-// 					alert("Transaksi gagal silahkan cobalagi");
-// 				},
-// 				onClose: (result: unknown) => {
-// 					console.log(result);
-// 					alert("Transaksi gagal silahkan cobalagi");
-// 				},
-// 			});
+		const bodyData = {
+			bookingDate: new Date(),
+			customerId: user?.id,
+			flightId: data?.flightDetail?.id,
+			price: totalPrice,
+			seatId: data?.seatDetail?.id,
+			departureCityCode: data?.flightDetail?.departureCityCode,
+			destinationCityCode: data?.flightDetail?.destinationCityCode,
+		};
 
-// 			setIsLoading(false);
-// 		} catch (error) {
-// 			setIsLoading(false);
-// 			console.log(error);
-// 		}
-// 	};
+		try {
+			setIsLoading(true);
+			const transaction = await transactionMutate.mutateAsync(bodyData);
 
-// 	return {
-// 		payTransaction,
-// 		isLoading,
-// 	};
-// };
+			// handle midtrans
+			window.snap.pay(transaction.midtrans.token, {
+				onSuccess: (result: unknown) => {
+					console.log(result);
+					router.push("/success-checkout");
+				},
+				onPending: (result: unknown) => {
+					console.log(result);
+					router.push("/success-checkout");
+				},
+				onError: (result: unknown) => {
+					console.log(result);
+					alert("Transaksi gagal silahkan cobalagi");
+				},
+				onClose: (result: unknown) => {
+					console.log(result);
+					alert("Transaksi gagal silahkan cobalagi");
+				},
+			});
 
-// export default useTransaction;
+			setIsLoading(false);
+		} catch (error) {
+			setIsLoading(false);
+			console.log(error);
+		}
+	};
+
+	return {
+		payTransaction,
+		isLoading,
+	};
+};
+
+export default useTransaction;
